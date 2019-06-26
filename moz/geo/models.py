23ab160cc1067520e15c96a052115821a421b239
Andriy Tykhonov <atykhonov@gmail.com>
2019-06-27 01:33:30 +0300

    Add `Provider` model.

--- moz/geo/models.py
+++ moz/geo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from languages.fields import LanguageField
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Provider(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=100)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    language = LanguageField()
