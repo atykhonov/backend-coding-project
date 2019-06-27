e83ba23122df51322aa8a00ff824967537959729
Andriy Tykhonov <atykhonov@gmail.com>
2019-06-28 01:54:28 +0300

    Prepare infrastructure for the unit tests.

---
+++ moz/moz/test_settings.py
@@ -0,0 +1,32 @@
+SECRET_KEY = 'fake-key'
+
+ROOT_URLCONF = 'moz.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'phonenumber_field',
+    'languages',
+    'geo.apps.GeoConfig',
+    'rest_framework',
+    'rest_framework_gis',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'mbcp_db',
+        'USER': 'mbcp_user',
+        'PASSWORD': 'xsq3SHWtR82Cu9Ca6s',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'mbcp_tests',
+        }
+    }
+}
